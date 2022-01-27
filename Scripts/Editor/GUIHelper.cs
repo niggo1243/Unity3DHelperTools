d14cb6f1f1ffd79ff11f7b24e6a9695bdfdc024f
Nigg <nikodem.grzonkowski@msm.digital>
2022-01-27 18:34:33 +0100

    fixed build error and created a ui helper class in nikoshelpers

---
+++ Scripts/Editor/GUIHelper.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace NikosAssets.Helpers.Editor
+{
+    public static class GUIHelper
+    {
+        public static void GUILine(Color color, float height = 1, float widthMultiplier = 1)
+        {
+            Rect rect = EditorGUILayout.GetControlRect(false, height );
+            rect.height = height;
+
+            float fullWidth = rect.width;
+            rect.width *= widthMultiplier;
+
+            float diffWidth = fullWidth - rect.width;
+            rect.x += diffWidth * .5f;
+
+            EditorGUI.DrawRect(rect, color);
+        }
+    }
+}
