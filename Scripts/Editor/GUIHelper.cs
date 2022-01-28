7fb787350457202323a672f739b8085a704f6c81
Nigg <nikodem.grzonkowski@msm.digital>
2022-01-28 18:19:40 +0100

    #152 fixed and refactored the colorbox creation, as well as moved some resources to nikoshelpers

--- Scripts/Editor/GUIHelper.cs
+++ Scripts/Editor/GUIHelper.cs
@@ -5,6 +5,32 @@ namespace NikosAssets.Helpers.Editor
 {
     public static class GUIHelper
     {
+        private static GUIStyle _colorBoxStyle;
+
+        public static GUIStyle ColorBoxStyle
+        {
+            get
+            {
+                if (_colorBoxStyle == null)
+                {
+                    _colorBoxStyle = new GUIStyle();
+                    _colorBoxStyle.border = new RectOffset(1, 1, 1, 1);
+                    _colorBoxStyle.normal.background = GUIHelper.WhiteTexBorder1PX;
+                }
+                
+                return _colorBoxStyle;
+            }
+        }
+        
+        public static Texture2D BlackTex1PX { get { return _blackTex1PX != null ? _blackTex1PX : _blackTex1PX = Resources.Load<Texture2D>("black1px"); } }
+        private static Texture2D _blackTex1PX;
+        
+        public static Texture2D WhiteTexBorder1PX { get { return _whiteTexBorder1PX != null ? _whiteTexBorder1PX : _whiteTexBorder1PX = Resources.Load<Texture2D>("white_box_border1px"); } }
+        private static Texture2D _whiteTexBorder1PX;
+        
+        public static Texture2D WhiteTexBorder2PX { get { return _whiteTexBorder2PX != null ? _whiteTexBorder2PX : _whiteTexBorder2PX = Resources.Load<Texture2D>("white_box_border2px"); } }
+        private static Texture2D _whiteTexBorder2PX;
+        
         public static void GUILine(Color color, float height = 1, float widthMultiplier = 1)
         {
             Rect rect = EditorGUILayout.GetControlRect(false, height );
@@ -17,6 +43,16 @@ namespace NikosAssets.Helpers.Editor
             rect.x += diffWidth * .5f;
 
             EditorGUI.DrawRect(rect, color);
+        }
+        
+        public static void DrawColorBox(Color boxColor, Color prevGUIColor, int boxWidth, int boxHeight, RectOffset margin)
+        {
+            ColorBoxStyle.margin = margin;
+
+            GUI.color = boxColor;
+            GUILayout.Label(GUIContent.none, ColorBoxStyle, GUILayout.Width(boxWidth), GUILayout.Height(boxHeight));
+            //revert to prev color
+            GUI.color = prevGUIColor;
         }
     }
 }
