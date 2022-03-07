eb4d7fa329c29df00a4bf570e4dc02e4e47325d2
Nigg <nikodem.grzonkowski@msm.digital>
2022-03-07 01:02:22 +0100

    refactoring dynamic/ automatic asset creation items drawer but needs custom name and sort mapping

--- Scripts/ReflectionHelper.cs
+++ Scripts/ReflectionHelper.cs
@@ -7,7 +7,7 @@ namespace NikosAssets.Helpers
 {
     public static class ReflectionHelper
     {
-        public static Type GetFoundChildOfAncestor(Type descendant, Type ancestorToFind)
+        public static Type FindChildOfAncestor(Type descendant, Type ancestorToFind)
         {
             if (descendant == null || ancestorToFind == null)
                 return null;
@@ -16,10 +16,10 @@ namespace NikosAssets.Helpers
                 //found the child
                 return descendant;
 
-            return GetFoundChildOfAncestor(descendant.BaseType, ancestorToFind);
+            return FindChildOfAncestor(descendant.BaseType, ancestorToFind);
         }
         
-        public static Type GetFoundGenericTypeOfParentClassAtIndex (Type descendant, Type parentToFind, int i = 0)
+        public static Type FindGenericTypeOfParentClassAtIndex (Type descendant, Type parentToFind, int i = 0)
         {
             if (descendant == null || parentToFind == null)
                 return null;
@@ -43,40 +43,109 @@ namespace NikosAssets.Helpers
                 }
                 else
                 {
-                    return ReflectionHelper.GetFoundGenericTypeOfParentClassAtIndex(descendant.BaseType, parentToFind, i);
+                    return ReflectionHelper.FindGenericTypeOfParentClassAtIndex(descendant.BaseType, parentToFind, i);
                 }
             }
 
             return null;
         }
 
-        public static List<Type> FindAllDerivedTypesAcrossAllAssemblies<T>()
+        public static List<Type> FindAllDerivedTypesAcrossAllAssemblies(Type rootType)
         {
             Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
             List<Type> types = new List<Type>();
             
             foreach (Assembly assembly in assemblies)
             {
-                types.AddRange(FindAllDerivedTypes<T>(assembly));
+                types.AddRange(FindAllDerivedTypes(rootType, assembly));
             }
 
             return types;
         }
         
-        public static List<Type> FindAllDerivedTypes<T>()
+        public static List<Type> FindAllDerivedTypes(Type rootType)
         {
-            return FindAllDerivedTypes<T>(Assembly.GetAssembly(typeof(T)));
+            return FindAllDerivedTypes(rootType, Assembly.GetAssembly(rootType));
         }
 
-        public static List<Type> FindAllDerivedTypes<T>(Assembly assembly)
+        public static List<Type> FindAllDerivedTypes(Type rootType, Assembly assembly)
         {
-            var derivedType = typeof(T);
             return assembly
                 .GetTypes()
                 .Where(t =>
-                    t != derivedType &&
-                    derivedType.IsAssignableFrom(t)
+                    t != rootType &&
+                    rootType.IsAssignableFrom(t)
                 ).ToList();
         } 
+        
+        /// <summary>
+        /// Doesnt work with interfaces
+        /// </summary>
+        /// <returns></returns>
+        public static List<Type> FindDirectChildrenOfTypeAcrossAllAssemblies(Type parentType)
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            List<Type> types = new List<Type>();
+            
+            foreach (Assembly assembly in assemblies)
+            {
+                types.AddRange(FindDirectChildrenOfType(parentType, assembly));
+            }
+
+            return types;
+        }
+        
+        /// <summary>
+        /// Doesnt work with interfaces
+        /// </summary>
+        /// <returns></returns>
+        public static List<Type> FindDirectChildrenOfType(Type parentType)
+        {
+            return FindDirectChildrenOfType(parentType, Assembly.GetAssembly(parentType));
+        }
+
+        /// <summary>
+        /// Doesnt work with interfaces
+        /// </summary>
+        /// <param name="parentType"></param>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        public static List<Type> FindDirectChildrenOfType(Type parentType, Assembly assembly)
+        {
+            return assembly
+                .GetTypes()
+                .Where(t =>
+                    t.BaseType == parentType
+                ).ToList();
+        }
+        
+        public static List<Type> FindInterfacesAcrossAllAssemblies(Type interfaceToContain, int maxInterfaces = 1)
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            List<Type> types = new List<Type>();
+            
+            foreach (Assembly assembly in assemblies)
+            {
+                types.AddRange(FindInterfaces(interfaceToContain, assembly, maxInterfaces));
+            }
+
+            return types;        
+        }
+        
+        public static List<Type> FindInterfaces(Type interfaceToContain, int maxInterfaces = 1)
+        {
+            return FindInterfaces(interfaceToContain, Assembly.GetAssembly(interfaceToContain), maxInterfaces);
+        }
+        
+        public static List<Type> FindInterfaces(Type interfaceToContain, Assembly assembly, int maxInterfaces = 1)
+        {
+            return assembly
+                .GetTypes()
+                .Where(t =>
+                {
+                    Type[] interfaces = t.GetInterfaces();
+                    return t.IsInterface && interfaces.Length == maxInterfaces && interfaces.Contains(interfaceToContain);
+                }).ToList();
+        }
     }
 }
