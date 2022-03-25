4d0123c174a8082e168d363e29fbe780f28bc3ab
Nigg <nikodem.grzonkowski@msm.digital>
2022-03-25 14:09:33 +0100

    helpers small refactoring of extensions

--- Scripts/Extensions/ObjectUtils.cs
+++ Scripts/Extensions/ObjectUtils.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 using Object = UnityEngine.Object;
 
 namespace NikosAssets.Helpers.Extensions
 {
-    public static class ClassUtils
+    public static class ObjectUtils
     {
         /// <summary> Perform a deep Copy of the object. Binary Serialization is used to perform the copy </summary>
         /// Reference Article http://www.codeproject.com/KB/tips/SerializedObjectCloner.aspx
@@ -54,38 +49,6 @@ namespace NikosAssets.Helpers.Extensions
             T instance = Object.Instantiate(scriptableObject);
             instance.name = scriptableObject.name; // remove (Clone) from name
             return instance;
-        }
-
-        public static ulong GetUInt64Hash(this string text)
-        {
-            return text.GetUInt64Hash(SHA256.Create());
-        }
-        
-        /// <summary>
-        ///
-        /// author: https://stackoverflow.com/a/50364956
-        /// </summary>
-        /// <param name="text"></param>
-        /// <param name="hasher"></param>
-        /// <returns></returns>
-        public static ulong GetUInt64Hash(this string text, HashAlgorithm hasher)
-        {
-            using (hasher)
-            {
-                var bytes = hasher.ComputeHash(Encoding.Default.GetBytes(text));
-                Array.Resize(ref bytes, bytes.Length + bytes.Length % 8); //make multiple of 8 if hash is not, for exampel SHA1 creates 20 bytes. 
-                return Enumerable.Range(0, bytes.Length / 8) // create a counter for de number of 8 bytes in the bytearray
-                    .Select(i => BitConverter.ToUInt64(bytes, i * 8)) // combine 8 bytes at a time into a integer
-                    .Aggregate((x, y) =>x ^ y); //xor the bytes together so you end up with a ulong (64-bit int)
-            }
-        }
-
-        public static string CropString(this string text, int maxLength = 40, string cropSymbols = "...")
-        {
-            if (maxLength >= text.Length)
-                return text;
-            
-            return text.Substring(0, maxLength) + cropSymbols;
         }
     }
 }
